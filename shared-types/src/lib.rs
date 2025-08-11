1ff341cb6c88072c90be2555acd907539f7c80d8
Max <max.den.oudsten@gmail.com>
2025-08-11 21:55:38 +0200

    refactor(shared-types): Removed template code from lib.rs

--- shared-types/src/lib.rs
+++ shared-types/src/lib.rs
@@ -165,14 +165,3 @@ pub struct Settings {
     pub legacy_mode: bool,
     pub preserve_legacy_output_format: bool,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
