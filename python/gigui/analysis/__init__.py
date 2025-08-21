78b6925040d742be99a8063977f13f730decf282
Max <max.den.oudsten@gmail.com>
2025-08-21 17:04:09 +0200

    refactor: removed python files altogether

--- python/gigui/analysis/__init__.py
+++
@@ -1,7 +0,0 @@
-"""GitInspectorGUI Analysis Components.
-
-This package provides analysis components including blame analysis
-and performance monitoring.
-"""
-
-__all__ = []
--- python/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""GitInspectorGUI Test Suite.
-
-This package contains all test files for the GitInspectorGUI backend.
-"""
-
-__all__ = []
