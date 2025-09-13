1fd90270fbcd69a27eb0d448b524b71dcab9bf41
Max <max.den.oudsten@gmail.com>
2025-09-13 20:51:31 +0200

    feat(shared_types): adds all types according to module overview

--- gi_core/src/shared_types.rs
+++ gi_core/src/shared_types.rs
@@ -4,76 +4,81 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Settings {
     pub repositories: Vec<String>,
-    pub depth: usize,
+    pub search_depth: usize,
+    pub ignored_file_extensions: Vec<String>,
+
 }
 
 impl Default for Settings {
     fn default() -> Self {
         Settings {
             repositories: vec![],
-            depth: 3,
+            search_depth: 3,
+            ignored_file_extensions: vec![],
         }
     }
 }
 
-// Old stuff
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct AuthorStat {
+pub struct Author {
     pub name: String,
     pub email: String,
-    pub commits: i32,
-    pub insertions: i32,
-    pub deletions: i32,
-    pub files: i32,
-    pub percentage: f64,
-    pub age: String, // Optional, default ""
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct FileStat {
+pub struct File {
     pub name: String,
+    pub extension: String,
     pub path: String,
-    pub lines: i32,
-    pub commits: i32,
-    pub authors: i32,
-    pub percentage: f64,
+    pub file_size: usize,
+    pub lines: usize,
+    pub metrics: Metrics,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct BlameEntry {
-    pub file: String,
-    pub line_number: i32,
-    pub author: String,
-    pub commit: String,
+pub struct Commit {
+    pub hash: String,
+    pub author: Author,
     pub date: String,
-    pub content: String,
+    pub message: String,
+    pub files_changed: Vec<File>,
+    pub metrics: Metrics,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct RepositoryResult {
+pub struct Repository {
     pub name: String,
     pub path: String,
-    pub authors: Vec<AuthorStat>,
-    pub files: Vec<FileStat>,
-    pub blame_data: Vec<BlameEntry>,
+    pub authors: Vec<Author>,
+    pub commits: Vec<Commit>,
+    pub files: Vec<File>,
+    pub metrics: Metrics,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+/// The Metrics struct stores metrics in the context of the struct it is used in.
+/// For example, in the context of a Repository, it stores overall repository metrics.
+/// All metrics are optional and can be None if not calculated or not applicable.
+pub struct Metrics {
+    pub loc: Option<usize>,
+    pub sloc: Option<usize>,
+    pub cloc: Option<usize>,
+    pub insertions: Option<usize>,
+    pub deletions: Option<usize>,
+    pub total_commits: Option<usize>,
+    pub total_authors: Option<usize>,
+    pub total_files: Option<usize>,
+}
+
+pub struct AnalysisParameters {
+    pub from_time: Option<String>, // e.g., "2023-01-01"
+    pub to_time: Option<String>,   // e.g., "2023-12-31"
+    pub from_commit: Option<String>,
+    pub to_commit: Option<String>,
+    // TODO: Check if Vec<&T> is appropriate here or if we should use Vec<String> or similar
+    pub exclude_authors: Vec<&Author>,
+    pub exclude_files: Vec<&File>,       
+}
+
 pub struct AnalysisResult {
-    pub repositories: Vec<RepositoryResult>,
-    pub success: bool,
-    pub error: Option<String>,
+    pub repository: Repository,
+    pub authors: Vec<Author>,
+    pub commits: Vec<Commit>,
+    pub files: Vec<File>,
+    pub metrics: Metrics,
 }
-
-// #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-// pub struct Settings {
-//     // Repository and Input Settings
-//     pub input_fstrs: Vec<String>,
-//     pub depth: i32,
-//     pub subfolder: String,
-
-//     // File Analysis Settings
-//     pub n_files: i32,
-//     pub include_files: Vec<String>,
-//     // ...rest of fields...
-// }
