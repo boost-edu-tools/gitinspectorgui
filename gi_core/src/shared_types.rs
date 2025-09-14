51e06d3fce870d4e8e5005f9ef892c947ea32f65
Max <max.den.oudsten@gmail.com>
2025-09-14 22:04:42 +0200

    refactor(shared_types): reorders code

--- gi_core/src/shared_types.rs
+++ gi_core/src/shared_types.rs
@@ -1,38 +1,5 @@
 use serde::{Serialize, Deserialize};
 
-/// The Settings struct holds configuration settings for the analysis.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct Settings {
-    pub repositories: Vec<String>,
-    pub search_depth: usize,
-    pub ignored_file_extensions: Vec<String>,
-
-}
-
-impl Default for Settings {
-    fn default() -> Self {
-        Settings {
-            repositories: vec![],
-            search_depth: 3,
-            ignored_file_extensions: vec![],
-        }
-    }
-}
-
-/// The Author struct represents an author of commits in the repository.
-pub struct Author {
-    pub name: String,
-    pub email: String,
-}
-
-/// The Line struct represents a line in a file with its associated data.
-pub struct Line {
-    pub number: usize,
-    pub content: String,
-    pub author: Author,
-    pub commit_hash: String,
-    pub date: String,
-}
 
 /// The File struct represents a file in the repository with its associated data.
 pub struct File {
@@ -44,15 +11,16 @@ pub struct File {
     pub metrics: Metrics,
 }
 
-/// The Commit struct represents a git commit with its associated data.
-pub struct Commit {
-    pub hash: String,
+
+/// The Line struct represents a line in a file with its associated data.
+pub struct Line {
+    pub number: usize,
+    pub content: String,
     pub author: Author,
+    pub commit_hash: String,
     pub date: String,
-    pub message: String,
-    pub files_changed: Vec<File>,
-    pub metrics: Metrics,
 }
+
 
 /// The Repository struct represents a git repository with its associated data.
 pub struct Repository {
@@ -63,6 +31,24 @@ pub struct Repository {
     pub files: Vec<File>,
     pub metrics: Metrics,
 }
+
+
+/// The Commit struct represents a git commit with its associated data.
+pub struct Commit {
+    pub hash: String,
+    pub author: Author,
+    pub date: String,
+    pub message: String,
+    pub files_changed: Vec<File>,
+    pub metrics: Metrics,
+}
+
+/// The Author struct represents an author of commits in the repository.
+pub struct Author {
+    pub name: String,
+    pub email: String,
+}
+
 
 /// The Metrics struct stores metrics in the context of the struct it is used in.
 /// For example, in the context of a Repository, it stores overall repository metrics.
@@ -77,6 +63,7 @@ pub struct Metrics {
     pub total_authors: Option<usize>,
     pub total_files: Option<usize>,
 }
+
 
 /// The AnalysisParameters struct defines parameters for filtering the analysis of a repository.
 pub struct AnalysisParameters {
@@ -102,6 +89,7 @@ impl Default for AnalysisParameters {
     }
 }
 
+
 /// The AnalysisResult struct encapsulates the results of analyzing a repository.
 /// It includes the full repository (unfiltered), lists of authors, commits, and files involved in the analysis,
 /// as well as aggregated metrics, which can be filtered based on the analysis parameters.
@@ -111,4 +99,24 @@ pub struct AnalysisResult {
     pub commits: Vec<Commit>,
     pub files: Vec<File>,
     pub metrics: Metrics,
+}
+
+
+/// The Settings struct holds configuration settings for the analysis.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Settings {
+    pub repositories: Vec<String>,
+    pub search_depth: usize,
+    pub ignored_file_extensions: Vec<String>,
+
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            repositories: vec![],
+            search_depth: 3,
+            ignored_file_extensions: vec![],
+        }
+    }
 }
